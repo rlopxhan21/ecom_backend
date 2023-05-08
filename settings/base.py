ab05e7b2825be3e8d290e8f4e8e117b95867bd15
rlopxhan21 <rlopxhan21@gmail.com>
2023-05-08 11:01:44 +1000

    Add Static Root

--- settings/base.py
+++ settings/base.py
@@ -125,6 +125,8 @@ USE_TZ = True
 # STATIC_ROOT
 STATIC_URL = 'static/'
 
+STATIC_ROOT = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
